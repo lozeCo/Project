5206f3cf3465ee3e9457442b0a5a8a6eab67ca44
Edgar Lopez <jatateon@gmail.com>
2019-08-23 10:25:30 -0500

    ajustes cors+

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware(['auth:api', 'cors'])->get('/apiPagos/{use}/{password}', 'apiPagos@index');
+// Route::middleware(['cors'])->get('/apiPagos/{use}/{password}', 'apiPagos@index');
+Route::get('/apiPagos/{use}/{password}', 'apiPagos@index');
 
 Route::get('/projects/{id?}', 'Project\ProjectController@show');
 //japhom rutas
